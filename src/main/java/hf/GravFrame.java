a1a4c6a7acdb502a6090d76b280297b5d559de52
ZDave <david@zoller.dev>
2024-10-20 18:02:29 +0200

    Added gitignore

--- src/main/java/hf/GravFrame.java
+++ src/main/java/hf/GravFrame.java
@@ -7,13 +7,14 @@ public class GravFrame extends JFrame {
     public GravFrame() {
         this.setTitle("grav");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(800, 600);
-        this.setVisible(true);
+        this.setSize(1000, 600);
         this.setResizable(false);
 
         ImageIcon icon = new ImageIcon("src/main/resources/icon.png");
         this.setIconImage(icon.getImage());
         this.setBackground(new Color(0xE0E1E3));
+        
+        this.setVisible(true);
     }
 
 }
