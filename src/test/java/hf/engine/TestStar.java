a125d50f6f0c18f1007daed784e4b373c0d7a5be
ZD4ve <david@zoller.dev>
2024-11-12 13:14:11 +0100

    test accel claculation

---
+++ src/test/java/hf/engine/TestStar.java
@@ -0,0 +1,37 @@
+package hf.engine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestStar {
+    Star a;
+    Star b;
+    Star c;
+    SimParameters simParams;
+
+    @BeforeEach
+    void initParams() {
+        SimParameters.resetInstance();
+        simParams = new SimParameters();
+        a = new Star(0);
+        b = new Star(1);
+        c = new Star(2);
+    }
+
+    @Test
+    void testCalculateAccelaration() {
+        // Arrange
+        a.position = new Vec2(-1, 1);
+        b.position = new Vec2(1, 1);
+        c.position = new Vec2(1, -1);
+
+        // Act
+        Vec2 acceleration = b.calculateAccelaration(a, c);
+
+        // Assert
+        assertEquals(new Vec2(-1, -1).normalise(), acceleration.normalise());
+    }
+
+}
